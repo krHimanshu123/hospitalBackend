7e694acaff9cd1c621ae743926337477818424a8
Himanshu Kumar <162711386+krHimanshu123@users.noreply.github.com>
2026-01-15 23:35:12 +0530

    Add import statement for JpaRepository

--- src/main/java/com/klu/hospital/repository/UserRepository.java
+++ src/main/java/com/klu/hospital/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.klu.hospital.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.stereotype.Repository;
 
 import com.klu.hospital.entity.User;
